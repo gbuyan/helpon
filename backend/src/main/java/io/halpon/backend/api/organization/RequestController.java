37cbca9c5aab68eddada2b8b333d5f147ce53585
gleb <buyanking@gmail.com>
2018-10-15 21:30:54 +0300

    Bean naming fixes

--- backend/src/main/java/io/halpon/backend/api/organization/RequestController.java
+++ backend/src/main/java/io/halpon/backend/api/organization/RequestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("organizationRequestController")
 @RequestMapping("/api/v1/organization/request")
 public class RequestController {
 
